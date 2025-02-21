50f13035fb8307295c663941e2770b99cc098d0d
Sebastian Livoni <29739749+sebastianlivoni@users.noreply.github.com>
2025-02-21 22:12:41 +0100

    Shows QT in autofill when using macdeployqt

--- src/autofill/CredentialProviderViewController.mm
+++ src/autofill/CredentialProviderViewController.mm
@@ -1,11 +1,18 @@
 #include "CredentialProviderViewController.h"
 #include "AutoFillService.h"
 #include <AuthenticationServices/AuthenticationServices.h>
+#include <QtWidgets/QWidget>
+#include <QApplication>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QVBoxLayout>
+#include <QtPlugin>
+#include <QDebug>
 
 @implementation CredentialProviderViewController
 
-- (void)prepareCredentialListForServiceIdentifiers:
-    (NSArray<ASCredentialServiceIdentifier *> *)serviceIdentifiers {
+- (void) prepareCredentialListForServiceIdentifiers:(NSArray<ASCredentialServiceIdentifier *> *) serviceIdentifiers {
+
 }
 
 - (void)provideCredentialWithoutUserInteractionForRequest:
@@ -50,6 +57,36 @@
           [NSError errorWithDomain:ASExtensionErrorDomain
                               code:ASExtensionErrorCodeUserInteractionRequired
                           userInfo:nil]];
+}
+
+- (void) prepareInterfaceForExtensionConfiguration {
+  NSLog(@"Hello from prepare interface 1");
+
+  setenv("QT_DEBUG_PLUGINS", "1", 1);
+
+  int argc = 0;
+  char *argv[] = { nullptr };
+  QApplication *qtApp = new QApplication(argc, argv); // Måske QGuiApplication?
+
+  NSLog(@"Hello from prepare interface 2");
+
+  QPushButton* btn = new QPushButton("Some Button");
+  QLabel* lbl = new QLabel("QTGui");
+  QVBoxLayout* layout = new QVBoxLayout();
+  layout->addWidget(lbl);
+  layout->addWidget(btn);
+
+  QWidget* window = new QWidget();
+  window->setLayout(layout);
+  window->show();
+  window->resize(490, 200);
+
+  NSView* newView = (__bridge NSView*)reinterpret_cast<void*>(window->winId());
+
+  newView.translatesAutoresizingMaskIntoConstraints = NO;
+  [newView setFrameSize:NSMakeSize(490, 200)];
+  
+  [self.view addSubview:newView];
 }
 
 @end
